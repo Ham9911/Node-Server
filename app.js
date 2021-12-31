b82d7ccc4d99ff341ef5cc1097e98a8e46caa7a3
ham9911 <mhammad8633@gmail.com>
2021-12-31 20:40:09 +0500

    node server

--- app.js
+++ app.js
@@ -1,35 +1,23 @@
-const http=require('http');
-const fs=require('fs');
-const path=require('path');
-const filePath=path.join(__dirname,'data.txt');
-const server=http.createServer((req,res)=>{
-    console.log(req.url);
-    if(req.url==='/'){
-    res.write('Hello');
-res.end();    
-    }
-    else if(req.url==='/form'){
-        res.setHeader("Content-Type","text/html")
-        res.write("<form action='/submit' method='POST'>Input <input name='input'/>Input 2 <input name='input2'/><button>Submit</button></form>")
-        res.end();
-    }
-    else if(req.url==='/submit'){
-        let data ='';
-        req.on("data",(chunk)=>{
-            data+=chunk;
-        })
-        req.on("end",()=>{
-            fs.readFile(filePath,'utf8',(err,oldData)=>{
-               const newData=oldData+"\n"+data;
-                fs.writeFile(filePath,newData,()=>{
-                    console.log("Data Saved");
-                })
-            })
-            
-        })
-        res.write("Data Received")
-        res.end();
-    }
+const express=require("express");
+const res = require("express/lib/response");
+const app= express();
+const formRoutes=require("./routes/form");
+const bodyParser = require("body-parser");
+const path=require("path");
 
+app.use(bodyParser.urlencoded({extended:false}));
+
+
+app.use(express.static(path.join(__dirname,"public")));
+
+app.use((req,res,next)=>{
+    console.log(req.url);
+    next();
+});
+
+
+app.use("/form",formRoutes)
+app.use("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"index.html"))
 })
-server.listen(3000);
+app.listen(3000);
